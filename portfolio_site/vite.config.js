6787da4b4abfde8c28b6b17324c96a758b7ad1de
Jonas <jonaskasakewitch32@gmail.com>
2025-05-21 16:36:54 -0300

    att

--- portfolio_site/vite.config.js
+++ portfolio_site/vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: './',
+  build: {
+    outDir: 'dist',         // garante que tudo vá direto para dist/
+    emptyOutDir: true,      // limpa antes de cada build
+  }
+})
