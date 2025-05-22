fb819b97566d854cf295522d0fe33c813d2e6add
Jonas <jonaskasakewitch32@gmail.com>
2025-05-22 10:43:23 -0300

    alterando vite

--- portfolio_site/vite.config.js
+++ portfolio_site/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Portfolio/',  // <<--- esta linha é ESSENCIAL
   build: {
-    outDir: 'dist',         // garante que tudo vá direto para dist/
-    emptyOutDir: true,      // limpa antes de cada build
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
